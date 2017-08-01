cafbc7c5321287603aedcf5e13303efc34147306
linfeng365 <linfeng365@MacBook-pro-13.local>
2017-08-01 20:07:11 +0800

    ex01
    ex01

---
+++ ex01.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+print "Hello World!"
+print "我是林峰。" # 注释
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I"said" do not touch this.'
